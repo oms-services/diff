a526818889f6f3cbedc90bd89b1b5bb67ca49db1
kianenigma <Kian.peymani@gmail.com>
2018-12-04 15:37:35 +0100

    adds simple decorator to allow dual method and validate input

--- app.py
+++ app.py
@@ -1,28 +1,54 @@
 
+from functools import wraps
 from flask import Flask, request, jsonify
 
 from gdiff import GDiff 
+
 app = Flask(__name__)
 gdiff = GDiff()
 
-@app.route("/diff/raw/", methods=['POST'])
+def validate_diff(fn):
+  @wraps(fn)
+  def wrapper(*args, **kwargs):
+    if request.method == 'POST':
+      if not 't1' in request.json or not 't2' in request.json:
+        return jsonify({'error': 't1 and t2 must be present as data.'}), 400
+      else:
+        request.diff_data = request.json
+    elif request.method == 'GET':
+      if not 't1' in request.args or not 't2' in request.args:
+        jsonify({'error': 't1 and t2 must be present as data.'}), 400
+      else:
+        request.diff_data = {}
+        request.diff_data['t1'] = request.args.get('t1')
+        request.diff_data['t2'] = request.args.get('t2')
+    
+    return fn(*args, **kwargs)
+
+  return wrapper
+
+@app.route("/diff/", methods=['POST', 'GET'])
+@validate_diff
 def diff_raw():
-  content = request.json
+  content = request.diff_data
+  t1 = content['t1']
+  t2 = content['t2']
+  return jsonify({
+    'diff': gdiff.dmp.diff_main(t1, t2),
+    'type': 'raw'
+  }), 200
+  
+
+@app.route("/diff/raw/", methods=['POST', 'GET'])
+@validate_diff
+def diff():
+  content = request.diff_data
   t1 = content['t1']
   t2 = content['t2']
   return jsonify({
     'diff': gdiff.diff(t1, t2),
     'type': 'raw'
-  })
-
-@app.route("/diff/", methods=['POST'])
-def diff():
-  content = request.json
-  t1 = content['t1']
-  t2 = content['t2']
-  diff = gdiff.diff(t1, t2)
-  print(diff.split('\n'))
-  return "ok"
+  }), 200
 
 
 if __name__ == "__main__":
--- gdiff.py
+++ gdiff.py
@@ -4,10 +4,10 @@ class GDiff:
   def __init__(self):
     self.dmp = diff_match_patch()
   
-  def _patch(self, t1, t2):
+  def _patch(self, t1, t2) -> object:
     return self.dmp.patch_make(t1, t2)
 
-  def diff(self, t1, t2):
+  def diff(self, t1, t2) -> str:
     patch = self._patch(t1, t2)
     diff = self.dmp.patch_toText(patch)    
     return diff
